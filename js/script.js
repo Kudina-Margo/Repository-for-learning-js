bb8fe77f5c9f1f3f4068c73ebe9170f5108ff576
Margarita <ritakydina@gmail.com>
2022-02-05 21:51:36 +0200

    first version my program

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log(1);
+let number = 5;
+const name = 'Rita';
+var isMarried = false; //var лучше не использовать, происходит всплытие
+number = 10;
+console.log(number);
+const obj = {
+    name: Rita,
+    lastname: Kudina,
+    age: 19
+};
